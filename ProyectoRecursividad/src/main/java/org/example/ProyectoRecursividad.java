69dcc376f56b923fa2c7cedf93950cbc0aef31a2
DESKTOP-AI1DKKS\Dani <danielleludovicoo@gmail.com>
2025-01-15 18:53:50 +0100

    preoyecto recursividad

--- ProyectoRecursividad/src/main/java/org/example/ProyectoRecursividad.java
+++ ProyectoRecursividad/src/main/java/org/example/ProyectoRecursividad.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ProyectoRecursividad {
 
     static Scanner teclado = new Scanner(System.in);
+    static char palabra[];
 
     public static void main(String[] args) {
         imprimir();
@@ -28,28 +30,74 @@ public class ProyectoRecursividad {
 
             switch (eleccion) {
                 case 1:
+                    System.out.println("Dime un número");
                     int numero=pedirnumero();
-                    System.out.println(digitos());
-
+                    System.out.println("El numero es: "+digitos(numero));
                     break;
                 case 2:
-
+                    System.out.println("Dime la base:");
+                    int base=pedirnumero();
+                    System.out.println("Dime el exponente:");
+                    int exp=pedirnumero();
+                    System.out.println("El numero es: "+potencias(base,exp));
                     break;
                 case 3:
-                    System.out.println("Quieres invertir una cadena o un numero");
-                    String opcion = teclado.next();
+                    System.out.println("dime el modo que quieres elegir:");
+                    System.out.println("\t 1.numero");
+                    System.out.println("\t 2.cadena");
+                    int opcion = teclado.nextInt();
+                    switch (opcion){
+                        case 1:
+                            System.out.println("Has elegido invertir numero");
+                            System.out.println("Dime un numero:");
+                            int inv = pedirnumero();
+                            invertido(inv);
+                            break;
+                        case 2:
+                            System.out.println("Has elegido invertir cadena");
+                            System.out.println("Dime una cadena:");
+                            String p = teclado.next();
+                            palabra=p.toCharArray();
+                            System.out.print(Arrays.toString(palabra));
+                            invertir_palabra(palabra.length-1);
+                            break;
+                        default:
+                            System.out.println("ERROR");
+                            break;
+                    }
                     break;
                 case 4:
+                    System.out.println("Dime un numero:");
+                    int bin = pedirnumero();
+                    if (esBinario(bin)){
+                        System.out.println("El numero "+bin + " es binario");
+                    }else {
+                        System.out.println("El numero "+bin+" no es binario");
+                    }
 
                     break;
                 case 5:
+                    System.out.println("dime un numero para convertir en binario:");
+                    int conv= pedirnumero();
+                    System.out.println("El numero en binario es "+convertir(conv));
 
                     break;
                 case 6:
+                    System.out.println("Dime una palabra");
+                    String pal = teclado.next();
+                    palabra=pal.toLowerCase().toCharArray();
+                    System.out.print(Arrays.toString(palabra));
+                    if (estaOrdenada(0)){
+                        System.out.println("palabra ordenada");
+                    }else{
+                        System.out.println("palabra desordenada");
+                    }
 
                     break;
                 case 7:
-
+                    System.out.println("Dime un numero:");
+                    int numro=pedirnumero();
+                    System.out.println(suma(numro));
                     break;
                 default:
                     System.out.println("ERROR: la opción no es válida");
@@ -74,9 +122,8 @@ public class ProyectoRecursividad {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
 
-    public static void pedirnumero(int numero){
-        System.out.println("Introduce un número:");
-        int dig = teclado.nextInt();
+    public static int pedirnumero(){
+        return teclado.nextInt();
     }
 
     public static int digitos(int n){
@@ -104,5 +151,57 @@ public class ProyectoRecursividad {
             invertido(numero/10);
         }
     }
+    public static void invertir_palabra (int contador){
+        if (contador==0){
+            System.out.println(palabra[contador]);
+        }else {
+            System.out.println(palabra[contador]);
+            invertir_palabra(--contador);
+        }
+    };
+
+    public static boolean estaOrdenada(int posicion) {
+        if (posicion < palabra.length - 1) {
+            if (palabra[posicion] <= palabra[posicion + 1]) {
+                return estaOrdenada(++posicion);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    public static boolean esBinario (int numero){
+        if (numero>9){
+            if (numero%10==0|| numero%10==1){
+                return esBinario(numero/10);
+            }else{
+                return false;
+            }
+        }
+        if (numero==0||numero==1){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static String convertir(int numero){
+
+        if (numero==1||numero==0){
+            return Integer.toString(numero);
+        }else {
+            return convertir(numero/2)+numero%2;
+        }
+    }
+
+    public static int suma(int numero) {
+        if (numero == 1) {
+            return 1;
+        }
+        return numero + suma(numero - 1);
+    }
+
 
 }
+
